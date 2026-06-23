75c45a207d30a0ac7d6c91b35df31dcf3e4acac9
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-22 22:58:01 -0400

    Add Kestówv0.5.0/ipc/rpc.rb

---
+++ Kestówv0.5.0/ipc/rpc.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - ipc/rpc.rb
+#
+# Remote Procedure Call simulation.
+# Registers RPC features.
+
+module Kestowv
+  module Ipc
+    module Rpc
+      @methods = {}
+      @mutex   = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:ipc_rpc)
+          Boot.set_bit(:ipc_rpc)
+        end
+
+        def register(name, &block)
+          @mutex.synchronize { @methods[name] = block }
+        end
+
+        def call(name, *args)
+          @methods[name]&.call(*args)
+        end
+
+        def to_a
+          @methods.keys
+        end
+
+        def stats
+          {
+            feature: :ipc_rpc,
+            methods: @methods.size
+          }
+        end
+      end
+    end
+  end
+end
