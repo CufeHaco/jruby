457405c0bf18902efb82e7bf31a4e5190b49e6a3
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-23 01:15:38 -0400

    Add Kestówv0.5.0/net/interface.rb

---
+++ Kestówv0.5.0/net/interface.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - net/interface.rb
+#
+# Network interface management.
+# Registers interface features.
+
+module Kestowv
+  module Net
+    module Interface
+      @interfaces = {}
+      @mutex      = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:net_interface)
+          Boot.set_bit(:net_interface)
+        end
+
+        def create(name, device)
+          @mutex.synchronize do
+            @interfaces[name] = { device: device, state: :down, addresses: [] }
+          end
+        end
+
+        def up(name)
+          @mutex.synchronize { @interfaces[name][:state] = :up   if @interfaces[name] }
+        end
+
+        def down(name)
+          @mutex.synchronize { @interfaces[name][:state] = :down if @interfaces[name] }
+        end
+
+        def add_address(name, address)
+          @mutex.synchronize { @interfaces[name][:addresses] << address if @interfaces[name] }
+        end
+
+        def to_a
+          @interfaces
+        end
+
+        def stats
+          {
+            feature: :net_interface,
+            count:   @interfaces.size
+          }
+        end
+      end
+    end
+  end
+end
