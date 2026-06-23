b1f7feffd1df0911fe067a074676e05cf6d3512c
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-23 00:47:56 -0400

    Add Kestówv0.5.0/mm/oom.rb

---
+++ Kestówv0.5.0/mm/oom.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - mm/oom.rb
+#
+# Out-of-memory handling simulation.
+# Registers OOM features.
+
+module Kestowv
+  module Mm
+    module Oom
+      @score = {}
+      @mutex = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:mm_oom)
+          Boot.set_bit(:mm_oom)
+        end
+
+        def set_score(pid, score)
+          @mutex.synchronize { @score[pid] = score }
+        end
+
+        def get_score(pid)
+          @score[pid] || 0
+        end
+
+        def to_a
+          @score
+        end
+
+        def stats
+          {
+            feature: :mm_oom,
+            tracked: @score.size
+          }
+        end
+      end
+    end
+  end
+end
