801b5a6fafc356ef4582ab3d337c247401803119
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-22 22:56:50 -0400

    Add Kestówv0.5.0/ipc/bus.rb

---
+++ Kestówv0.5.0/ipc/bus.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - ipc/bus.rb
+#
+# Event bus for inter-component communication.
+# Registers bus features.
+
+module Kestowv
+  module Ipc
+    module Bus
+      @subscribers = {}
+      @mutex       = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:ipc_bus)
+          Boot.set_bit(:ipc_bus)
+        end
+
+        def subscribe(event, &block)
+          @mutex.synchronize do
+            @subscribers[event] ||= []
+            @subscribers[event] << block
+          end
+        end
+
+        def publish(event, *args)
+          (@subscribers[event] || []).each { |h| h.call(*args) }
+        end
+
+        def to_a
+          @subscribers.keys
+        end
+
+        def stats
+          {
+            feature: :ipc_bus,
+            events:  @subscribers.size
+          }
+        end
+      end
+    end
+  end
+end
