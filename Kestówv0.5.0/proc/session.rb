a8b43bf2b351506f3befdeb09d130ebe1ead1276
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-22 23:48:39 -0400

    Add Kestówv0.5.0/proc/session.rb

---
+++ Kestówv0.5.0/proc/session.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - proc/session.rb
+#
+# Session and process group management.
+# Registers session features.
+
+module Kestowv
+  module Proc
+    module Session
+      @sessions = {}
+      @mutex = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:proc_session)
+          Boot.set_bit(:proc_session)
+        end
+
+        def create(sid)
+          @mutex.synchronize do
+            @sessions[sid] = { pgids: [] }
+          end
+        end
+
+        def to_a
+          @sessions
+        end
+
+        def stats
+          {
+            feature: :proc_session,
+            sessions: @sessions.size
+          }
+        end
+      end
+    end
+  end
+end
