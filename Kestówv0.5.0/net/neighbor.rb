beaa3b18f5b6220280a11649fb621f996fe0d3a0
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-23 01:40:56 -0400

    Add Kestówv0.5.0/net/neighbor.rb

---
+++ Kestówv0.5.0/net/neighbor.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - net/neighbor.rb
+#
+# Neighbor / ARP table simulation.
+# Registers neighbor features.
+
+module Kestowv
+  module Net
+    module Neighbor
+      @neighbors = {}
+      @mutex     = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:net_neighbor)
+          Boot.set_bit(:net_neighbor)
+        end
+
+        def add(ip, mac, interface)
+          @mutex.synchronize do
+            @neighbors[ip] = { mac: mac, interface: interface }
+          end
+        end
+
+        def to_a
+          @neighbors
+        end
+
+        def stats
+          {
+            feature: :net_neighbor,
+            count:   @neighbors.size
+          }
+        end
+      end
+    end
+  end
+end
