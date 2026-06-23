76bfc64bb163d76608369a2a940b961415286f5f
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-22 23:37:47 -0400

    Add Kestówv0.5.0/fs/buffer.rb

---
+++ Kestówv0.5.0/fs/buffer.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - fs/buffer.rb
+#
+# Buffer cache simulation.
+# Registers buffer features.
+
+module Kestowv
+  module Fs
+    module Buffer
+      @buffers = {}
+      @mutex   = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:fs_buffer)
+          Boot.set_bit(:fs_buffer)
+        end
+
+        def get(block)
+          @mutex.synchronize { @buffers[block] }
+        end
+
+        def put(block, data)
+          @mutex.synchronize { @buffers[block] = data }
+        end
+
+        def to_a
+          @buffers.keys
+        end
+
+        def stats
+          {
+            feature: :fs_buffer,
+            count:   @buffers.size
+          }
+        end
+      end
+    end
+  end
+end
