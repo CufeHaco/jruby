85fbd97195a923ca6acdc6225f59cddbc5891c7a
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-22 23:51:10 -0400

    Add Kestówv0.5.0/ipc/transport.rb

---
+++ Kestówv0.5.0/ipc/transport.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - ipc/transport.rb
+#
+# IPC transport abstraction.
+# Registers transport features.
+
+module Kestowv
+  module Ipc
+    module Transport
+      @transports = {}
+      @mutex      = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:ipc_transport)
+          Boot.set_bit(:ipc_transport)
+        end
+
+        def register(name, impl)
+          @mutex.synchronize { @transports[name] = impl }
+        end
+
+        def get(name)
+          @transports[name]
+        end
+
+        def to_a
+          @transports.keys
+        end
+
+        def stats
+          {
+            feature:    :ipc_transport,
+            transports: @transports.size
+          }
+        end
+      end
+    end
+  end
+end
