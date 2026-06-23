da055703e1072f603bb63bb01fb967bfec3fca01
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-23 01:42:59 -0400

    Add Kestówv0.5.0/net/dns.rb

---
+++ Kestówv0.5.0/net/dns.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - net/dns.rb
+#
+# DNS simulation.
+# Registers DNS features.
+
+module Kestowv
+  module Net
+    module Dns
+      @records = {}
+      @mutex = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:net_dns)
+          Boot.set_bit(:net_dns)
+        end
+
+        def add_record(name, ip)
+          @mutex.synchronize { @records[name] = ip }
+        end
+
+        def resolve(name)
+          @records[name]
+        end
+
+        def to_a
+          @records
+        end
+
+        def stats
+          {
+            feature: :net_dns,
+            records: @records.size
+          }
+        end
+      end
+    end
+  end
+end
