8b40efd4a4d8b3d545d3fabfb3d21600baf96fe9
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-23 01:17:21 -0400

    Add Kestówv0.5.0/net/route.rb

---
+++ Kestówv0.5.0/net/route.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - net/route.rb
+#
+# Routing table simulation.
+# Registers route features.
+
+module Kestowv
+  module Net
+    module Route
+      @routes = []
+      @mutex  = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:net_route)
+          Boot.set_bit(:net_route)
+        end
+
+        def add(destination, gateway, interface)
+          @mutex.synchronize do
+            @routes << { destination: destination, gateway: gateway, interface: interface }
+          end
+        end
+
+        def to_a
+          @routes
+        end
+
+        def stats
+          {
+            feature: :net_route,
+            routes:  @routes.size
+          }
+        end
+      end
+    end
+  end
+end
