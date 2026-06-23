38c299301b4daf1476c4ac5820e191c30c4f3d5e
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-23 01:42:05 -0400

    Add Kestówv0.5.0/net/filter.rb

---
+++ Kestówv0.5.0/net/filter.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - net/filter.rb
+#
+# Packet filter simulation.
+# Registers filter features.
+
+module Kestowv
+  module Net
+    module Filter
+      @rules = []
+      @mutex = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:net_filter)
+          Boot.set_bit(:net_filter)
+        end
+
+        def add_rule(action, condition)
+          @mutex.synchronize do
+            @rules << { action: action, condition: condition }
+          end
+        end
+
+        def to_a
+          @rules
+        end
+
+        def stats
+          {
+            feature: :net_filter,
+            rules: @rules.size
+          }
+        end
+      end
+    end
+  end
+end
