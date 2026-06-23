767b18c3631e332401f99cc3334a9be17133394d
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-23 01:42:07 -0400

    Add Kestówv0.5.0/net/arp.rb

---
+++ Kestówv0.5.0/net/arp.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - net/arp.rb
+#
+# ARP simulation.
+# Registers ARP features.
+
+module Kestowv
+  module Net
+    module Arp
+      @table = {}
+      @mutex = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:net_arp)
+          Boot.set_bit(:net_arp)
+        end
+
+        def add(ip, mac)
+          @mutex.synchronize { @table[ip] = mac }
+        end
+
+        def resolve(ip)
+          @table[ip]
+        end
+
+        def to_a
+          @table
+        end
+
+        def stats
+          {
+            feature: :net_arp,
+            entries: @table.size
+          }
+        end
+      end
+    end
+  end
+end
