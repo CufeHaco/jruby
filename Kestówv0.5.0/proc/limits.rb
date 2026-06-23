408eebb47b9b2ec78e2942917257a92010fa7865
CufeHaco <crazyrabbitking89@gmail.com>
2026-06-22 23:44:25 -0400

    Add Kestówv0.5.0/proc/limits.rb

---
+++ Kestówv0.5.0/proc/limits.rb
@@ -0,0 +1,44 @@
+# frozen_string_literal: true
+
+# Kestówv 0.5.0 - proc/limits.rb
+#
+# Resource limits (rlimits).
+# Registers limits features.
+
+module Kestowv
+  module Proc
+    module Limits
+      @limits = {}
+      @mutex  = Mutex.new
+
+      class << self
+        def register_features
+          Boot.register(:proc_limits)
+          Boot.set_bit(:proc_limits)
+        end
+
+        def set(pid, resource, soft, hard)
+          @mutex.synchronize do
+            @limits[pid] ||= {}
+            @limits[pid][resource] = { soft: soft, hard: hard }
+          end
+        end
+
+        def get(pid, resource)
+          @limits.dig(pid, resource)
+        end
+
+        def to_a
+          @limits
+        end
+
+        def stats
+          {
+            feature:   :proc_limits,
+            processes: @limits.size
+          }
+        end
+      end
+    end
+  end
+end
